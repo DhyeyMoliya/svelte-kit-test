b9db0c7dda711eaaca97f9a0da29bd0503a448c8
Dhyey Moliya <dhyeymoliya@gmail.com>
2020-12-10 21:16:47 +0530

    Updated deps and config according to new version

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,10 @@ module.exports = {
     // Consult https://github.com/sveltejs/svelte-preprocess
     // for more information about preprocessors
     preprocess: sveltePreprocess(),
-    // By default, `npm run build` will create a standard Node app.
-    // You can create optimized builds for different platforms by
-    // specifying a different adapter
-    adapter: '@sveltejs/adapter-static'
+    kit: {
+		// By default, `npm run build` will create a standard Node app.
+		// You can create optimized builds for different platforms by
+		// specifying a different adapter
+		adapter: '@sveltejs/adapter-node'
+	}
 };
